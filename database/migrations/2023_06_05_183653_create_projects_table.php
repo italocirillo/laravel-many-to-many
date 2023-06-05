dec3ddd22607cea346231c31dcfd6062b2ff9e86
italocirillo <italocirillo@gmail.com>
2023-06-05 20:44:47 +0200

    MIGRATION FATTA

--- database/migrations/2023_06_05_183653_create_projects_table.php
+++ database/migrations/2023_06_05_183653_create_projects_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 200)->unique();
+            $table->string('slug');
+            $table->text('content')->nullable();
             $table->timestamps();
         });
     }
